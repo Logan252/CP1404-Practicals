eb6b90fb680c66fe15c10b0b6a769cc0c324528a
Logan252 <114317521+Logan252@users.noreply.github.com>
2023-06-10 23:17:48 +1000

    Add files via upload

---
+++ prac_02/password_stars.py
@@ -0,0 +1,25 @@
+"""
+Copy password_stars.py that you wrote earlier from Sandbox into your prac_02 folder and commit
+(with a message like "Add password check program")
+"""
+
+min_length = 8
+
+
+def main():
+    password = get_password(min_length)
+    print_asterisks(password)
+
+
+def get_password(minimum_length):
+    password = input("Please enter password of at least {} characters: ".format(min_length))
+    while len(password) < minimum_length:
+        password = input("Please enter password of at least {} characters: ".format(min_length))
+    return password
+
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
+
+
+main()
