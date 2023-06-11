0406e290417e99cf8b4754c68ac57f1c81c7cdff
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-11 22:04:33 +1000

    from old file thats not connected to github. Error has been fixed for following practicals (week 3).

--- prac_02/Scores.py
+++ prac_02/Scores.py
@@ -2,14 +2,29 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-# self marking, suggested answer has if instead of while loop, if this matters please provide feedback during marking
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+
+
+def main():
     score = float(input("Enter score: "))
-if 50 < score < 90:
-    print("Passable")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Bad")
+    print(give_grade(score))
+    # don't want an Invalid score, below comments is exploratory
+    # while give_grade(score) == "Invalid score":
+    # score = float(input("Enter score: "))
+    # print(give_grade(score))
+
+
+def give_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+
+    elif 50 <= score < 90:
+        return "Passable"
+
+    elif score >= 90:
+        return "Excellent"
+
+    else:
+        return "Bad"
+
+
+main()
