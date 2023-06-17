e548bf95f198f09b3ae56463fc4c4cb6887a8252
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-17 21:15:31 +1000

    ask until valid input is given using exception handling

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+ValueError occurs when a non integer is entered during the input for the numerator and denominator.
+2. When will a ZeroDivisionError occur?
+Zero divison error occurs only when input from the denominator = 0
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+# standard exception handling pattern
+is_valid_input = False
+while not is_valid_input:
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+        if denominator == 0:
+            print("denominator cannot be 0")
+        else:
+            is_valid_input = True
+            fraction = numerator / denominator
+            # Instead of having 2 print lines
+            print(f"{fraction}\nFinished")
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
