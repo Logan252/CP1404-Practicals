0b879d0752cc69d454edebac7467cfeb3a3e3a8d
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-18 11:43:06 +1000

    add first two numbers in txt file

--- prac_03/files.py
+++ prac_03/files.py
@@ -12,11 +12,36 @@ out_file.close()
 in_file = open("name.txt", "r")
 name = in_file.read()
 in_file.close()
-print("name is", name)
+print("Name entered is", name)
 
 # wanted to test 'a'. 'w' overwrites the file, 'a' append to the file.
-out_file = open("name.txt", "a")
-name = input("Please enter your name")
-print(name, file=out_file)
-out_file.close()
+# out_file = open("name.txt", "a")
+# name = input("the second name entered is")
+# print(name, file=out_file)
+# out_file.close()
 
+# using "with"
+with open("name.txt", "r") as in_file:
+    name = in_file.read().strip()
+print("Name entered is", name)
+
+in_file = open("numbers.txt", "r")
+total = 0
+# question asks for first 2 numbers, for loop made sense
+for line in range(2):
+    numbers = int(in_file.readline())
+    total += numbers
+    # print(numbers) was just used to see what numbers were being selected during testing
+print(total)
+in_file.close()
+
+# question asks for all numbers
+in_file = open("numbers.txt", "r")
+total = 0
+
+for line in range(in_file):
+    numbers = int(in_file.readline())
+    total += numbers
+    # print(numbers) was just used to see what numbers were being selected during testing
+print(total)
+in_file.close()
