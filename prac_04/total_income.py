5288b0be089b9784f3e9dca974b72fd02926a476
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-23 21:53:42 +1000

    remove number_of_months in print_report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,15 +13,17 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+# doesn't need number_of_months?
+def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month, income in enumerate(incomes, 1):
         income = incomes[month - 1]
         total += income
+        # not asked for in "things to do" but should this line also be put into an f-string?
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
