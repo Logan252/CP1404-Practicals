8c5935cec0b40c703cd3b67c1bc72b8206d72b6c
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-23 21:57:42 +1000

    remove unnecessary comments and update print_report to an f-string

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -16,15 +16,13 @@ def main():
     print_report(incomes)
 
 
-# doesn't need number_of_months?
 def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, 1):
         income = incomes[month - 1]
         total += income
-        # not asked for in "things to do" but should this line also be put into an f-string?
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
