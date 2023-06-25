6b2e3720cb7375dd84ec4aadb225b6779f5405c6
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-25 15:58:36 +1000

    refactor  numbers_per_line constant

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,15 +5,17 @@ import random
 
 MINIMUM_LINES = 1
 MAXIMUM_LINES = 45
-LINE_NUMBERS = 6
+NUMBERS_PER_LINE = 6
 
 
 def main():
     number_of_quick_picks = int(input("How many quick picks?"))
     for i in range(number_of_quick_picks):
         quick_pick = []
-        for j in range(LINE_NUMBERS):
+        for j in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM_LINES, MAXIMUM_LINES)
             quick_pick.append(number)
             print(quick_pick)
+
+
 main()
