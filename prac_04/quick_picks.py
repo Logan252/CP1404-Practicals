760281406955ad2a7d2409b117ad13fdf696023e
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-25 16:15:28 +1000

    sort the quick picks from lowest to highest

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,8 +3,8 @@ The program then generates that many lines of output. Each line consists of 6 ra
 These values should be stored as CONSTANTS"""
 import random
 
-MINIMUM_LINES = 1
-MAXIMUM_LINES = 45
+LOWEST_QUICK_PICK_NUMBER = 1
+HIGHEST_QUICK_PICK_NUMBER = 45
 NUMBERS_PER_LINE = 6
 
 
@@ -13,11 +13,14 @@ def main():
     for i in range(number_of_quick_picks):
         quick_pick = []
         for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM_LINES, MAXIMUM_LINES)
+            number = random.randint(LOWEST_QUICK_PICK_NUMBER, HIGHEST_QUICK_PICK_NUMBER)
+            # to keep all line numbers different
             while number in quick_pick:
-                number = random.randint(MINIMUM_LINES, MAXIMUM_LINES)
+                number = random.randint(LOWEST_QUICK_PICK_NUMBER, HIGHEST_QUICK_PICK_NUMBER)
             quick_pick.append(number)
-        print(f"{quick_pick[0]} {quick_pick[1]} {quick_pick[2]} {quick_pick[3]} {quick_pick[4]} {quick_pick[5]}")
+        quick_pick.sort()
+        # number will never be higher than 2 digits
+        print(" ".join(f"{number:2}" for number in quick_pick))
 
 
 main()
