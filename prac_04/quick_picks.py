1a72cf88d1f8bcacd5c14f896dfaf066ea183588
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-06-25 16:06:50 +1000

    print the quick picks without the brackets and commas

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,7 +9,7 @@ NUMBERS_PER_LINE = 6
 
 
 def main():
-    number_of_quick_picks = int(input("How many quick picks?"))
+    number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
         quick_pick = []
         for j in range(NUMBERS_PER_LINE):
@@ -17,7 +17,7 @@ def main():
             while number in quick_pick:
                 number = random.randint(MINIMUM_LINES, MAXIMUM_LINES)
             quick_pick.append(number)
-        print(quick_pick)
+        print(f"{quick_pick[0]} {quick_pick[1]} {quick_pick[2]} {quick_pick[3]} {quick_pick[4]} {quick_pick[5]}")
 
 
 main()
