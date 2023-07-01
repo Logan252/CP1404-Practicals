975ae54d080cdee0852c76f909204adbe24503a3
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-02 09:30:11 +1000

    read wimdeldon file with encoding utf8

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,4 +1,20 @@
-in_file = open('wimbledon.csv')
-lines = [line.strip() for line in in_file.readlines()]
-print(lines)
-in_file.close()
+FILENAME = 'wimbledon.csv'
+
+
+def main():
+    read_file()
+
+
+def read_file():
+    data = []
+    in_file = open('FILENAME', encoding='utf-8')
+    lines = [line.strip() for line in in_file.readlines()]
+    data.append(lines)
+    winners = set(lines)
+    print(data)
+    print(winners)
+    print(len(winners))
+    in_file.close()
+
+
+main()
