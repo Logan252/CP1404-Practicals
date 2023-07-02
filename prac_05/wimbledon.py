5db8465b1e40e490765589747f2ce02acb05cd6d
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-02 11:21:52 +1000

    display countries and number of times won

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,9 +1,11 @@
 FILENAME = 'wimbledon.csv'
+data = []
 
 
 def main():
-    get_records()
+    records = get_records(FILENAME)
     champions_count, countries = process_records(records)
+    display_records(champions_count, countries)
 
 
 def process_records(records):
@@ -13,29 +15,35 @@ def process_records(records):
     countries = set()
 
     for record in records:
-        champion = record[0]
+        champion = record[2]
         country = record[1]
         if champion in champions_count:
             champions_count[champion] += 1
         else:
             champions_count[champion] = 1
+
         countries.add(country)
+
     return champions_count, countries
 
 
-def get_records():
+def get_records(filename):
     """get records from csv file"""
     records = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
             parts = line.strip().split(",")
             records.append(parts)
-    # print(records)
+    # print(records)      Test line
     return records
 
 
-# def display_records():
+def display_records(champions_count, countries):
+    print("Champions and how many times they've won")
+    for champion, count in champions_count.items():
+        print(f"{champion}: {count} time(s)")
+    print(f" These {count} countries have won wimbledon: \n{countries}")
 
 
 main()
