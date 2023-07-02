40bd8bfe249fb2797f323cfef4e148c31a07de3d
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-02 11:02:15 +1000

    process records and return how champion count

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,19 +2,40 @@ FILENAME = 'wimbledon.csv'
 
 
 def main():
-    read_file()
+    get_records()
+    champions_count, countries = process_records(records)
 
 
-def read_file():
-    data = []
-    in_file = open('FILENAME', encoding='utf-8')
-    lines = [line.strip() for line in in_file.readlines()]
-    data.append(lines)
-    winners = set(lines)
-    print(data)
-    print(winners)
-    print(len(winners))
-    in_file.close()
+def process_records(records):
+    """the champions and how many times they have won.
+    the countries of the champions in alphabetical order"""
+    champions_count = {}
+    countries = set()
+
+    for record in records:
+        champion = record[0]
+        country = record[1]
+        if champion in champions_count:
+            champions_count[champion] += 1
+        else:
+            champions_count[champion] = 1
+        countries.add(country)
+    return champions_count, countries
+
+
+def get_records():
+    """get records from csv file"""
+    records = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    # print(records)
+    return records
+
+
+# def display_records():
 
 
 main()
