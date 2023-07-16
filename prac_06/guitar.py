48c5569bb30415ee633d8d60e445b8b938a35bc3
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 13:23:56 +1000

    create class guitar

---
+++ prac_06/guitar.py
@@ -0,0 +1,9 @@
+"""Start time: 1:20
+    Finish time: """
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
