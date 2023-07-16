4acea41a53314a567a4423afb50f37e9d0b11596
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 13:46:08 +1000

    add test function for guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,5 +1,7 @@
 """Start time: 1:20
     Finish time: """
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
 
 
 class Guitar:
@@ -7,3 +9,17 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        return f"{self.name}, ({self.year}): ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Get the age of a guitar based on the CURRENT_YEAR."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if a Guitar is considered vintage based on age."""
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        return self.year < other.year
---
+++ prac_06/guitars.py
@@ -0,0 +1,8 @@
+"""start time will be in guitar file
+    guitar program"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
