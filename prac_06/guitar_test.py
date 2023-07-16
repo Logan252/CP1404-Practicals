83c07c63adf019ea09ef2d77983f396b07f938fb
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 13:51:45 +1000

    add comments for future studies

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,6 +5,7 @@ CURRENT_YEAR = 2023
 
 
 def run_tests():
+    # Test input from coder to check classes functionality
     name = "Gibson L-5 CES"
     year = 1922
     cost = 16035.40
@@ -12,9 +13,9 @@ def run_tests():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.9)
 
+    # The numbers 101 and 11 are from current year - age they come out.
     print(f"{guitar.name} get_age() - Expected {101}. Got {guitar.get_age()}")
     print(f"{other.name} get_age() - Expected {11}. Got {other.get_age()}")
-    print()
     print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
     print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
 
