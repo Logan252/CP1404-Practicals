24e5e2755163466c590dade362cff0f8b8b2acf8
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 13:48:26 +1000

    fix bug in run_test function

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -12,8 +12,8 @@ def run_tests():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2012, 1512.9)
 
-    print(f"{guitar.name} get_age() - Expected {95}. Got {guitar.get_age()}")
-    print(f"{other.name} get_age() - Expected {5}. Got {other.get_age()}")
+    print(f"{guitar.name} get_age() - Expected {101}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {11}. Got {other.get_age()}")
     print()
     print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
     print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
