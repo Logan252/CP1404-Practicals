cebf22d8514915e991c7790731d3a6006d231972
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 13:20:04 +1000

    print languages with typing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,10 +1,26 @@
-"""start time: 12:30"""
+"""start time: 12:30
+    End time: 1:16"""
 from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
-    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    # didn't require all languages but already had them in the program
+    java = ProgrammingLanguage("Java", "Dynamic", True, 1995)
     c_plus_plus = ProgrammingLanguage("C++", "Static", False, 1983)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    ruby = ProgrammingLanguage("Python", "Static", True, 1995)
+    ruby = ProgrammingLanguage("Ruby", "Static", True, 1995)
+
+    languages = [java, c_plus_plus, visual_basic, python, ruby]
+    print("Dynamic typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+    print("Static typed languages are:")
+    for language in languages:
+        if language.is_static():
+            print(language.name)
+
+
+main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,8 @@
 """Programming Language class"""
 
 
+# Some docstrings or comments may seem unnecessary through the program but are used for future study.
+
 class ProgrammingLanguage:
 
     def __init__(self, name, typing, reflection, year):
@@ -8,3 +10,38 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """is_dynamic() - which returns True/False if the programming language is dynamically typed or not Note: it's
+        really important that you understand this function will take no parameters (other than self). The information
+        is already stored inside the object, so you don't need to tell the object its own data. """
+        return self.typing == "Dynamic"
+
+    def is_static(self):
+        """Practice, not actually required for prac"""
+        return self.typing == "Static"
+
+
+def run_tests():
+    """Checked solutions once completed and noticed test was here,
+     is this standard practice or should be removed on a finished project?
+      Or I may have misunderstood the instructions"""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+    languages = [visual_basic, python, ruby]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
