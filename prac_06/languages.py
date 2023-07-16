2a458dfc8977f1db9101987e54e0a4a7fe107afa
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-16 12:48:57 +1000

    create class ProgrammingLanguage

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+"""start time: 12:30"""
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    c_plus_plus = ProgrammingLanguage("C++", "Static", False, 1983)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Python", "Static", True, 1995)
---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+"""Programming Language class"""
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
