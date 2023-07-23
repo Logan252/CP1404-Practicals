f9c70df9f92ab795910f79559503ed6495f27482
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-23 11:56:53 +1000

    Complete first part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,4 +1,6 @@
-"""Docstring message"""
+""" Script to add, organise and read guitars from CSV file.
+    Start time: 11:03
+    Finish time: ......"""
 
 
 class Guitar:
@@ -6,3 +8,36 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __lt__(self, other):
+        return self.year < other.year
+
+
+# Function to read guitars from the file and store them in a list of Guitar objects
+def read_guitars_from_file(filename):
+    guitars = []
+    with open(filename, 'r') as file:
+        for line in file:
+            name, year, cost = line.strip().split(',')
+            year = int(year)
+            cost = float(cost)
+            guitar = Guitar(name, year, cost)
+            guitars.append(guitar)
+    return guitars
+
+
+# Function to display guitars using a loop
+def display_guitars(guitars):
+    for guitar in guitars:
+        print(f"{guitar.name}, {guitar.year}, ${guitar.cost}")
+
+
+if __name__ == "__main__":
+    file_name = "guitars.csv"
+    guitars_list = read_guitars_from_file(file_name)
+
+    # Sort by oldest to newest
+    guitars_list.sort()
+
+    print("\nGuitars list sorted by year (oldest to newest):\n")
+    display_guitars(guitars_list)
