595a8bad7a2bc16b583a096f96ce10cec0a1eb3d
Logan Buttenshaw <69659934+Misorcandycorn@users.noreply.github.com>
2023-07-23 13:16:07 +1000

    add skeleton code

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,3 +8,11 @@ class Project:
         self.priority = priority
         self.estimate = estimate
         self.completion = completion
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, " \
+               f"priority {self.priority}, estimate: ${self.estimate:.2f}, " \
+               f"completion: {self.completion}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,3 +4,41 @@
 
 # Use the datetime module for the project start date
 import datetime
+from prac_07.project import Project
+
+FILE_NAME = "projects.txt"
+projects_list = load_projects(file_name)
+
+def main():
+
+
+
+
+def load_projects(filename):
+
+
+
+def save_projects(filename):
+
+
+
+def display_projects():
+
+
+
+
+
+def sort_projects():
+
+
+
+
+def add_project():
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
